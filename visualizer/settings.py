3adaa219aac2dd1d83ffda0c4ac79057360418bb
npopesku <nikolay.popesku@unbiased.co.uk>
2024-02-13 15:15:40 +0800

    added channels

--- visualizer/settings.py
+++ visualizer/settings.py
@@ -38,9 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
     # own
     "visualizer",
 ]
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+    },
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
