7cc33f7d15cf6ab0c212968bbc661fdf41300247
npopesku <nikolay.popesku@unbiased.co.uk>
2024-02-14 20:58:27 +0800

    chat app

--- visualizer/asgi.py
+++ visualizer/asgi.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
+from apps.chat.routing import websocket_urlpatterns
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'visualizer.settings')
 
 django_asgi_app = get_asgi_application()
@@ -19,6 +22,8 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        # Just HTTP for now. (We can add other protocols later.)
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
     }
 )
