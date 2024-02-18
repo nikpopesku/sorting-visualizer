c1d50c1f3f9f7c3b0b9708c3341a3cdd38654b2a
npopesku <nikolay.popesku@unbiased.co.uk>
2024-02-18 18:06:34 +0800

    simplified routing

--- apps/chat/urls.py
+++ apps/chat/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<str:room_name>/", views.room, name="room"),
+    path("", views.room, name="room"),
 ]
--- apps/chat/views.py
+++ apps/chat/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 
-def room(request, room_name):
-    return render(request, "chat/room.html", {"room_name": room_name})
+def room(request):
+    return render(request, "chat/room.html", {"room_name": "chat"})
