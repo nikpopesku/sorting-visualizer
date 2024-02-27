44205cdd5cf252274db213fae3a6ed4645a965c5
npopesku <nikolay.popesku@unbiased.co.uk>
2024-02-27 16:20:05 +0800

    added nginx and gunicorn files

---
+++ config/gunicorn/gunicorn_start.test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+NAME="test_poiskdoma_app"                               # Name of the application
+DJANGODIR=/var/www/tests/test_poiskdoma.net                   # Django project directory
+SOCKFILE=/var/www/tests/test_poiskdoma.net/run/gunicorn.sock  # we will communicte using this unix socket
+USER=spacer                                          # the user to run as
+GROUP=spacer                                        # the group to run as
+NUM_WORKERS=1                                       # how many worker processes should Gunicorn spawn
+DJANGO_SETTINGS_MODULE=config.settings.test             # which settings file should Django use
+DJANGO_WSGI_MODULE=poiskdoma.wsgi                     # WSGI module name
+
+echo "Starting $NAME as `whoami`"
+
+# Activate the virtual environment
+cd $DJANGODIR
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+# Create the run directory if it doesn't exist
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+exec `pipenv --venv`/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --user=$USER --group=$GROUP \
+  --bind=unix:$SOCKFILE \
+  --log-level=error \
+  --reload \
+  --log-file=-
