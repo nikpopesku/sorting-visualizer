4309da6835218857409e9ca38753f183caf12adc
npopesku <nikolay.popesku@unbiased.co.uk>
2024-03-13 16:48:07 +0800

    SV-6: one variant to fix the bug

--- deploy/build.sh
+++ deploy/build.sh
@@ -5,7 +5,7 @@ cd /var/www/sorting-visualizer
 git reset --hard
 git pull origin main
 pipenv install --python /usr/local/bin/python3.11
-DJANGO_SETTINGS_MODULE=config.settings.prod pipenv run daphne -b sorting-visualizer.dami.li -p 8001 visualizer.asgi:application
+DJANGO_SETTINGS_MODULE=config.settings.prod pipenv run daphne -e ssl:8001:privateKey=/var/www/sorting-visualizer/privkey.pem:certKey=/var/www/sorting-visualizer/cert.pem visualizer.asgi:application
 pipenv run python manage.py migrate --settings config.settings.prod
 pipenv run python manage.py collectstatic --noinput --settings config.settings.prod
 
