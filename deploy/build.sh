507b972bf5e26e591b3727dfdc2ed9a3958703b7
npopesku <nikolay.popesku@unbiased.co.uk>
2024-04-14 20:51:22 +0800

    SV-4: Changes in deploy file

--- deploy/build.sh
+++ deploy/build.sh
@@ -9,8 +9,8 @@ pipenv run python manage.py migrate --settings config.settings.prod
 pipenv run python manage.py collectstatic --noinput --settings config.settings.prod
 
 echo "Restart daemons"
-cat /home/spacer/pwd | sudo -S su -c "/etc/init.d/nginx restart"
-cat /home/spacer/pwd | sudo -S su -c "supervisorctl reread"
+/etc/init.d/nginx restart
+supervisorctl reread
 cat /home/spacer/pwd | sudo -S su -c "supervisorctl update sorting_visualizer_daphne"
 cat /home/spacer/pwd | sudo -S su -c "supervisorctl update sorting_visualizer_gunicorn"
 cat /home/spacer/pwd | sudo -S su -c "supervisorctl restart sorting_visualizer_daphne"
