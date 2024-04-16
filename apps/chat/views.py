80f95d5a52f446157c9362a5f594d284a0131508
npopesku <nikolay.popesku@unbiased.co.uk>
2024-04-16 14:55:38 +0800

    SV-12: fix appleid

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -1,3 +1,5 @@
+import time
+
 from django.shortcuts import render
 
 from apps.chat.generator import generate_array
@@ -8,7 +10,7 @@ def index(request):
     keys, values = generate_array(20, "")
 
     return render(request, "chat/index.html", {
-        "room_name": "chat",
+        "room_name": str(time.time()).replace('.', ''),
         "array": values.tolist(),
         "pyplot": get_image_data(keys, values)
     })
