5f39dec931e09b524a54e420caf820026f42df07
npopesku <nikolay.popesku@unbiased.co.uk>
2024-04-27 19:25:54 +0800

    SV-18: added performance cases

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -1,5 +1,6 @@
 import time
 
+import environ
 from django.shortcuts import render
 
 from apps.chat.generator import generate_array
@@ -8,9 +9,12 @@ from apps.chat.image import get_image_data
 
 def index(request):
     keys, values = generate_array(20, "")
+    env = environ.Env()
+    environ.Env.read_env()
 
     return render(request, "chat/index.html", {
         "room_name": str(time.time()).replace('.', ''),
         "array": values.tolist(),
-        "pyplot": get_image_data(keys, values)
+        "pyplot": get_image_data(keys, values),
+        "homelink": env('URL')
     })
