d3b6e717cc9fcfd8a6ca4299be20f571e8787530
npopesku <nikolay.popesku@unbiased.co.uk>
2024-04-29 21:15:19 +0800

    SV-11: implemented insertion sort

--- apps/sort/method/insertion.py
+++ apps/sort/method/insertion.py
@@ -5,14 +5,18 @@ def insertion_sort(array: list) -> list:
 
         # Compare key with each element on the left of it until an element smaller than it is found
         # For descending order, change key<array[j] to key>array[j].
+        was_changed = False
         while j >= 0 and key < array[j]:
             array[j + 1] = array[j]
             j = j - 1
 
-            return array
+            was_changed = True
 
         # Place key at after the element just smaller than it.
         array[j + 1] = key
+
+        if was_changed:
+            return array
 
     return array
 
